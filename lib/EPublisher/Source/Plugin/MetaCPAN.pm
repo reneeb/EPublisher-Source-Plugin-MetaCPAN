95f16cd0d462084d0e6c64db7b3793ce9e0c8d7e
Boris Daeppen <boris_daeppen@bluewin.ch>
2012-11-30 17:23:49 +0100

    bump version because dzil messed up at CPAN

--- lib/EPublisher/Source/Plugin/MetaCPAN.pm
+++ lib/EPublisher/Source/Plugin/MetaCPAN.pm
@@ -15,7 +15,7 @@ use EPublisher::Utils::PPI qw(extract_pod_from_code);
 
 our @ISA = qw( EPublisher::Source::Base );
 
-our $VERSION = 0.21;
+our $VERSION = 0.22;
 
 # implementing the interface to EPublisher::Source::Base
 sub load_source{
